f0359c0b583c7e8e0b11fb67314999642fe7fc5a
Kenny <dkhuong291@gmail.com>
2022-04-13 19:11:15 +0700

    Specify path

--- Package.swift
+++ Package.swift
@@ -27,6 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EncryptedAppStorage",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
     ]
 )
